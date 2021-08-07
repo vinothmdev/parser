138fa9e0c038d77e5786be6211ea1637c5b2d901
vinothmdev <vinoth.m.dev@gmail.com>
2021-08-07 17:31:39 -0400

    Uncommented test cases for unary expxression

--- src/parser-unary-expression.spec.ts
+++ src/parser-unary-expression.spec.ts
@@ -10,24 +10,24 @@ afterAll((done) => {
   done();
 });
 
-// test("Logical not", () => {
-//   const expected_value = {
-//     type: "Program",
-//     body: [
-//       {
-//         type: "ExpressionStatement",
-//         expression: {
-//           type: "UnaryExpression",
-//           operator: "!",
-//           argument: { type: "Identifier", name: "x" },
-//         },
-//       },
-//     ],
-//   };
+test("Logical not", () => {
+  const expected_value = {
+    type: "Program",
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "UnaryExpression",
+          operator: "!",
+          argument: { type: "Identifier", name: "x" },
+        },
+      },
+    ],
+  };
 
-//   const result = parser.parse(`!x;`);
-//   expect(result).toStrictEqual(expected_value);
-// });
+  const result = parser.parse(`!x;`);
+  expect(result).toStrictEqual(expected_value);
+});
 
 test("unary minus", () => {
   const expected_value = {
