ce40a1acd821cb0bc96d3b72ae4b83d22f478919
vinothmdev <vinoth.m.dev@gmail.com>
2021-08-07 14:54:07 -0400

    Additional AND/OR with parentheses

--- src/parser-logical-expresion.spec.ts
+++ src/parser-logical-expresion.spec.ts
@@ -578,3 +578,53 @@ test("x == 0 && x != 1 || y == 0", () => {
   );
   expect(result).toStrictEqual(expected_value);
 });
+
+test("x == 0 && (x != 1 || y == 0)", () => {
+  const expected_value = {
+    type: "Program",
+    body: [
+      {
+        type: "IfStatement",
+        test: {
+          type: "BinaryExpression",
+          operator: "&&",
+          left: {
+            type: "BinaryExpression",
+            operator: "==",
+            left: { type: "Identifier", name: "x" },
+            right: { type: "NumericLiteral", value: 0 },
+          },
+          right: {
+            type: "BinaryExpression",
+            operator: "||",
+            left: {
+              type: "BinaryExpression",
+              operator: "!=",
+              left: { type: "Identifier", name: "x" },
+              right: { type: "NumericLiteral", value: 1 },
+            },
+            right: {
+              type: "BinaryExpression",
+              operator: "==",
+              left: { type: "Identifier", name: "y" },
+              right: { type: "NumericLiteral", value: 0 },
+            },
+          },
+        },
+        consequent: {
+          type: "BlockStatement",
+          body: [],
+        },
+        alternate: null,
+      },
+    ],
+  };
+
+  const result = parser.parse(
+    `
+    if (x == 0 && (x != 1 || y == 0)) {
+    }
+    `
+  );
+  expect(result).toStrictEqual(expected_value);
+});
