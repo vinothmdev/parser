bf1f4fcb19604f82ef2399c7c18a29ce3077fae9
vinothmdev <vinoth.m.dev@gmail.com>
2021-08-07 17:36:49 -0400

    more test case for unary expression

--- src/parser-unary-expression.spec.ts
+++ src/parser-unary-expression.spec.ts
@@ -66,3 +66,60 @@ test("unary plus", () => {
   const result = parser.parse(`+x;`);
   expect(result).toStrictEqual(expected_value);
 });
+
+test("--x", () => {
+  const expected_value = {
+    type: "Program",
+    body: [
+      {
+        type: "ExpressionStatement",
+        expression: {
+          type: "UnaryExpression",
+          operator: "-",
+          argument: {
+            type: "UnaryExpression",
+            operator: "-",
+            argument: { type: "Identifier", name: "x" },
+          },
+        },
+      },
+    ],
+  };
+
+  const result = parser.parse(`--x;`);
+  expect(result).toStrictEqual(expected_value);
+});
+
+test("if(--x == 0)", () => {
+  const expected_value = {
+    type: "Program",
+    body: [
+      {
+        type: "IfStatement",
+        test: {
+          type: "BinaryExpression",
+          operator: "==",
+          left: {
+            type: "UnaryExpression",
+            operator: "-",
+            argument: {
+              type: "UnaryExpression",
+              operator: "-",
+              argument: { type: "Identifier", name: "x" },
+            },
+          },
+
+          right: { type: "NumericLiteral", value: 0 },
+        },
+        consequent: {
+          type: "BlockStatement",
+          body: [],
+        },
+        alternate: null,
+      },
+    ],
+  };
+
+  const result = parser.parse(`if(--x == 0){}`);
+  expect(result).toStrictEqual(expected_value);
+});
